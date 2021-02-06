04f7a681b63d15a88bad8a2055d1e4d233f4ce98
adrixo <adrianvalrom@gmail.com>
2021-02-06 01:07:28 +0100

    first

---
+++ precache-manifest.5ee711dfb1f2a1498fd05bbed8e1989f.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4fbb2232224c264fd60a10fb98f4adb",
+    "url": "/annotation-series.js"
+  },
+  {
+    "revision": "1fc29c5183bc713dcac9",
+    "url": "/css/app.55d1f296.css"
+  },
+  {
+    "revision": "0b09a5559d235b9890a5",
+    "url": "/css/chunk-vendors.65e75897.css"
+  },
+  {
+    "revision": "4cf2d64e44205fe628ddd534e1151b58",
+    "url": "/d3fc/.git/HEAD"
+  },
+  {
+    "revision": "f5e13bd413932fe993c43f623910d6ec",
+    "url": "/d3fc/.git/config"
+  },
+  {
+    "revision": "a0a7c3fff21f2aea3cfa1d0316dd816c",
+    "url": "/d3fc/.git/description"
+  },
+  {
+    "revision": "ce562e08d8098926a3862fc6e7905199",
+    "url": "/d3fc/.git/hooks/applypatch-msg.sample"
+  },
+  {
+    "revision": "579a3c1e12a1e74a98169175fb913012",
+    "url": "/d3fc/.git/hooks/commit-msg.sample"
+  },
+  {
+    "revision": "db4d62db2039e4bec38baa7567788284",
+    "url": "/d3fc/.git/hooks/fsmonitor-watchman.sample"
+  },
+  {
+    "revision": "2b7ea5cee3c49ff53d41e00785eb974c",
+    "url": "/d3fc/.git/hooks/post-update.sample"
+  },
+  {
+    "revision": "054f9ffb8bfe04a599751cc757226dda",
+    "url": "/d3fc/.git/hooks/pre-applypatch.sample"
+  },
+  {
+    "revision": "e4db8c12ee125a8a085907b757359ef0",
+    "url": "/d3fc/.git/hooks/pre-commit.sample"
+  },
+  {
+    "revision": "39cb268e2a85d436b9eb6f47614c3cbc",
+    "url": "/d3fc/.git/hooks/pre-merge-commit.sample"
+  },
+  {
+    "revision": "3c5989301dd4b949dfa1f43738a22819",
+    "url": "/d3fc/.git/hooks/pre-push.sample"
+  },
+  {
+    "revision": "56e45f2bcbc8226d2b4200f7c46371bf",
+    "url": "/d3fc/.git/hooks/pre-rebase.sample"
+  },
+  {
+    "revision": "2ad18ec82c20af7b5926ed9cea6aeedd",
+    "url": "/d3fc/.git/hooks/pre-receive.sample"
+  },
+  {
+    "revision": "2b5c047bdb474555e1787db32b2d2fc5",
+    "url": "/d3fc/.git/hooks/prepare-commit-msg.sample"
+  },
+  {
+    "revision": "517f14b9239689dff8bda3022ebd9004",
+    "url": "/d3fc/.git/hooks/update.sample"
+  },
+  {
+    "revision": "c77351f0cf4fccc3ff9e53675d36bf86",
+    "url": "/d3fc/.git/index"
+  },
+  {
+    "revision": "036208b4a1ab4a235d75c181e685e5a3",
+    "url": "/d3fc/.git/info/exclude"
+  },
+  {
+    "revision": "c3a71de4873790c4efa359c704449e18",
+    "url": "/d3fc/.git/logs/HEAD"
+  },
+  {
+    "revision": "c3a71de4873790c4efa359c704449e18",
+    "url": "/d3fc/.git/logs/refs/heads/master"
+  },
+  {
+    "revision": "c3a71de4873790c4efa359c704449e18",
+    "url": "/d3fc/.git/logs/refs/remotes/origin/HEAD"
+  },
+  {
+    "revision": "ea077082710c8f93ce3da6f565995a38",
+    "url": "/d3fc/.git/objects/pack/pack-2a0fc86760212c18105f210cb1bfe51665f99e88.idx"
+  },
+  {
+    "revision": "66bb5d48627b78ed7e12f7433b730afa",
+    "url": "/d3fc/.git/objects/pack/pack-2a0fc86760212c18105f210cb1bfe51665f99e88.pack"
+  },
+  {
+    "revision": "f47fca9e6cf049f8330300bf86b3f679",
+    "url": "/d3fc/.git/packed-refs"
+  },
+  {
+    "revision": "c9c5d226e92971408f5d4a1316e7fd2d",
+    "url": "/d3fc/.git/refs/heads/master"
+  },
+  {
+    "revision": "73a00957034783b7b5c8294c54cd3e12",
+    "url": "/d3fc/.git/refs/remotes/origin/HEAD"
+  },
+  {
+    "revision": "a3f4fd033b4b32433aadad0a82e03c0d",
+    "url": "/d3fc/.gitignore"
+  },
+  {
+    "revision": "cf991e32a62219c025294166fd43159a",
+    "url": "/data-downloader.js"
+  },
+  {
+    "revision": "0db4d72db94a5284794c08c40d145387",
+    "url": "/datasets/songs_reduced.csv"
+  },
+  {
+    "revision": "158cc343b04825a03a403df87319325f",
+    "url": "/img/neuralwallpaper2.158cc343.jpg"
+  },
+  {
+    "revision": "2fd11bd50fc5f9b04b95c520b2ef88b5",
+    "url": "/img/wallpaper.2fd11bd5.jpg"
+  },
+  {
+    "revision": "753b47b34878323532b27200201dc2f6",
+    "url": "/img/yellow-wallpaper.753b47b3.png"
+  },
+  {
+    "revision": "a23e1888d208cc8302c88ae9d4da0409",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ce32841cdabab5f8356480b507f94f8",
+    "url": "/js/app.14e94893.js"
+  },
+  {
+    "revision": "1fc29c5183bc713dcac9",
+    "url": "/js/app.3b6f0630.js"
+  },
+  {
+    "revision": "0b09a5559d235b9890a5",
+    "url": "/js/chunk-vendors.a0379954.js"
+  },
+  {
+    "revision": "0fc58047dcfb92e8564cdc3479441b29",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "520cccbc3d0249a492f5c20cac8422f2",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "0db4d72db94a5284794c08c40d145387",
+    "url": "/songs_reduced.csv"
+  },
+  {
+    "revision": "74d7a869a95f1242263ea14d8fd05595",
+    "url": "/streaming-tsv-parser.js"
+  },
+  {
+    "revision": "4803b426e8a04d0137049a6f2489cb20",
+    "url": "/umapdata-n15-d0.1_ext.tsv"
+  },
+  {
+    "revision": "3749c07e41ed9ce35c247c50ef170673",
+    "url": "/umapdata-n15-d0.25_ext.tsv"
+  },
+  {
+    "revision": "f0ac1044eea92f2f497eb84972d07034",
+    "url": "/umapdata-n15-d0.5_ext.tsv"
+  },
+  {
+    "revision": "2860dd1bd096f0b4bedce4b665f293fd",
+    "url": "/umapdata-n15-d0_ext.tsv"
+  },
+  {
+    "revision": "06ae698315efb905405e312c21739b6a",
+    "url": "/umapdata-n5-d0.1_ext.tsv"
+  },
+  {
+    "revision": "bf69115769bc8644661e9140e804613f",
+    "url": "/umapdata-n50-d0.1_ext.tsv"
+  },
+  {
+    "revision": "960ca57dce60ef21b867d5c6e338c7b3",
+    "url": "/umapsongs_reduced.tsv"
+  }
+]);
